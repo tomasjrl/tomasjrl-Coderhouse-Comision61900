f7009107d221d3f2b335ed264c99c708467e1998
tomasjrl <tstabilini@gmail.com>
2024-05-18 13:54:05 -0300

    scss colores light/dark mode

--- assets/js/toogle.js
+++ assets/js/toogle.js
@@ -1,21 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleMode = document.getElementById('toggleMode');
-
-  // Cambiar el modo y almacenar en localStorage
-  toggleMode.addEventListener('change', function() {
-    if (this.checked) {
-      document.body.classList.add('dark-mode');
-      localStorage.setItem('mode', 'dark');
-    } else {
-      document.body.classList.remove('dark-mode');
-      localStorage.setItem('mode', 'light');
-    }
-  });
-
-  // Recuperar el estado del modo almacenado en localStorage
-  const mode = localStorage.getItem('mode');
-  if (mode === 'dark') {
-    document.body.classList.add('dark-mode');
-    toggleMode.checked = true;
-  }
+document.getElementById('toggleMode').addEventListener('change', function() {
+  document.body.classList.toggle('light-mode', this.checked);
 });
