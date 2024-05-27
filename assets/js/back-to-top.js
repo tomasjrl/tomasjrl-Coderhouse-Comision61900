dd18df67220d3e5f2a3e797f3fb1de743010752c
tomasjrl <tstabilini@gmail.com>
2024-05-27 13:38:17 -0300

    prueba scroll smooth

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,25 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var btn = document.querySelector('.back-to-top');
-
-  // Smooth scroll to top
-  btn.addEventListener('click', function(e) {
-    e.preventDefault();
-    scrollToTop(1000); // Duración de la animación en milisegundos (por ejemplo, 1000 ms = 1 segundo)
-  });
-
-  // Smooth scrolling function
-  function scrollToTop(duration) {
-    const scrollHeight = window.scrollY,
-          scrollStep = Math.PI / (duration / 15),
-          cosParameter = scrollHeight / 2;
-    var scrollCount = 0,
-        scrollMargin,
-        scrollInterval = setInterval(function() {
-          if (window.scrollY !== 0) {
-            scrollCount = scrollCount + 1;
-            scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
-            window.scrollTo(0, (scrollHeight - scrollMargin));
-          } else clearInterval(scrollInterval);
-        }, 15);
-  }
-});
+function scrollToTop(){
+  window.scrollTo(0, 0);
+}
